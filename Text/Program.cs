6c9383dc0dcc05cc20cba2432716b42ab906dfea
lqd0292 <28313679866@qq.com>
2019-11-26 11:26:09 +0800

    模块 text 的初始签入

---
+++ Text/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace text
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
