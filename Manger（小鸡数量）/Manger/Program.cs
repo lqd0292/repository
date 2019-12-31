9153f15158075d88215bfc9964d3d35b30aab7f1
lqd0292 <58197203+lqd0292@users.noreply.github.com>
2019-12-31 12:14:55 +0800

    小鸡

---
+++ Manger（小鸡数量）/Manger/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Manger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j, i, k;
+            for (j = 0; j <= 20; j++)
+            {
+                for (i = 0; i <= 33; i++)
+                {
+                    for (k = 0; k <= 100; k++)
+                    {
+                        if ((j + i + k == 100) && (j * 5 + i * 3 + k / 3 == 100) && k % 3 == 0)
+                        {
+                            Console.WriteLine("{0},{1},{2}", j, i, k);
+                            Console.ReadLine();
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
