6048c6d95caa0ac805d74d2b13bffdc4fb50d6a8
lqd0292 <58197203+lqd0292@users.noreply.github.com>
2019-12-31 12:13:44 +0800

    扑克牌

---
+++ Playing（扑克牌）/Playing/PaperCard.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Playing
+{
+    class PaperCard
+    {
+
+        public string Type { get; set; }
+        public string Number { get; set; }
+        public PaperCard(string strType, string strNumber)
+        {
+            this.Type = strType;
+            this.Number = strNumber;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return string.Format("{0}{1}", this.Type, this.Number);
+            }
+
+            private set { }
+
+
+
+        }
+    }
+
+
+}
+
